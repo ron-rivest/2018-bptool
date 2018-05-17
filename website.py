b4dfc1ddbb7e071fc3606114407d529fdd1f3754
Mayuri Sridhar <mayuri@dhcp-18-189-118-157.dyn.MIT.EDU>
2018-05-17 08:38:13 -0400

    Add new page to website and fix error handling

--- website.py
+++ website.py
@@ -10,6 +10,9 @@ import bptool
 
 class BPToolPage:
 
+    def __init__(self):
+        self.samples_tool = SamplesToolPage()
+
     @cherrypy.expose
     def index(self):
         # Ask for the parameters required for the Bayesian Audit.
@@ -87,7 +90,11 @@ class BPToolPage:
             </a>.
 
             </p>
-
+            <h2> Escalating the Audit </h2>
+            <p> After running a single round of the audit, if the Bayesian risk limit
+            is not satisfied, we can increase the sample size of the audit across counties.
+            An easy way to do this is by using the <a href="./samples_tool">Sample Escalation Tool</a>.
+            </p>
 
 
             <form action="Query" method="GET">
@@ -203,7 +210,8 @@ class BPToolPage:
 
             Note: The Bayesian prior is represented by a pseudocount of one vote for
             each choice.  This may become an optional input parameter later.
-            </form>'''
+            </form>
+            '''
 
     @cherrypy.expose
     def Query(
@@ -233,7 +241,7 @@ class BPToolPage:
                         list, with no semicolons."
                     total_num_votes = [int(total)]
                     sample_tallies = [[int(k.strip()) for k in sample.split(',')]]
-                    assert(len(sample_tallies) == len(candidate_names)), "The length \
+                    assert(len(sample_tallies[0]) == len(candidate_names)), "The length \
                         of the sample tallies list, for a single county, should equal \
                         the number of candidates in the race."
                 else:
@@ -265,7 +273,7 @@ class BPToolPage:
                         # this is the default vote_for_n value, where each person votes for 1 candidate
                         vote_for_n=1
                     )
-                return get_html_results(candidate_names, win_probs)
+                return self.get_html_results(candidate_names, win_probs)
             except Exception as e:
                 return 'Please make sure all inputs are following the correct format and \
                 re-enter them <a href="./">here</a>.<br> \
@@ -273,57 +281,314 @@ class BPToolPage:
         else:
             return 'Please enter the sample and the total number of votes <a href="./">here</a>.'
 
-def get_html_results(candidate_names, win_probs):
-    """
-    Given list of candidate_names and win_probs pairs, print summary
-    of the Bayesian audit simulations.
+    def get_html_results(self, candidate_names, win_probs):
+        """
+        Given list of candidate_names and win_probs pairs, print summary
+        of the Bayesian audit simulations.
 
-    Input Parameters:
+        Input Parameters:
 
-    -candidate_names is an ordered list of strings, containing the name of
-    every candidate in the contest we are auditing.
+        -candidate_names is an ordered list of strings, containing the name of
+        every candidate in the contest we are auditing.
 
-    -win_probs is a list of pairs (i, p) where p is the fractional
-    representation of the number of trials that candidate i has won
-    out of the num_trials simulations.
+        -win_probs is a list of pairs (i, p) where p is the fractional
+        representation of the number of trials that candidate i has won
+        out of the num_trials simulations.
 
-    Returns:
+        Returns:
 
-    -String of HTML formatted results, which make a table on the online
-    BPTool.
-    """
+        -String of HTML formatted results, which make a table on the online
+        BPTool.
+        """
 
-    results_str = (
-        '<style> \
-        table, th, td { \
-                 border: 1px solid black; \
-        }\
-        </style>\
-        <h1> BPTOOL (Bayesian ballot-polling tool version 0.8) </h1>')
+        results_str = (
+            '<style> \
+            table, th, td { \
+                     border: 1px solid black; \
+            }\
+            </style>\
+            <h1> BPTOOL (Bayesian ballot-polling tool version 0.8) </h1>')
 
-    want_sorted_results = True
-    if want_sorted_results:
-        sorted_win_probs = sorted(
-            win_probs, key=lambda tup: tup[1], reverse=True)
-    else:
-        sorted_win_probs = win_probs
+        want_sorted_results = True
+        if want_sorted_results:
+            sorted_win_probs = sorted(
+                win_probs, key=lambda tup: tup[1], reverse=True)
+        else:
+            sorted_win_probs = win_probs
 
-    results_str += '<table style="width:100%">'
-    results_str += '<tr>'
-    results_str += ("<th>{:<24s}</th> <th>{:<s}</th>"
-          .format("Candidate name",
-                  "Estimated probability of winning a full manual recount"))
-    results_str += '</tr>'
-
-    for candidate_index, prob in sorted_win_probs:
-        candidate_name = str(candidate_names[candidate_index - 1])
+        results_str += '<table style="width:100%">'
         results_str += '<tr>'
-        results_str += ('<td style="text-align:center">{:<24s}</td>').format(candidate_name)
-        results_str += ('<td style="text-align:center">{:6.2f} %</td>').format(100*prob)
+        results_str += ("<th>{:<24s}</th> <th>{:<s}</th>"
+              .format("Candidate name",
+                      "Estimated probability of winning a full manual recount"))
         results_str += '</tr>'
-    results_str += '</table>'
-    results_str += '<p> Click <a href="./">here</a> to go back to the main page.</p>'
-    return results_str
+
+        for candidate_index, prob in sorted_win_probs:
+            candidate_name = str(candidate_names[candidate_index - 1])
+            results_str += '<tr>'
+            results_str += ('<td style="text-align:center">{:<24s}</td>').format(candidate_name)
+            results_str += ('<td style="text-align:center">{:6.2f} %</td>').format(100*prob)
+            results_str += '</tr>'
+        results_str += '</table>'
+        results_str += '<p> Click <a href="./">here</a> to go back to the main page.</p>'
+        return results_str
+
+class SamplesToolPage:
+
+    @cherrypy.expose
+    def index(self):
+        # Ask for the parameters to escalate an audit.
+        # Style parameters from 
+        # https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
+        return '''
+            <style>
+            input[type=text], select {
+                width: 50%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+            }
+            input[type=submit] {
+                width: 100%;
+                background-color: #4CAF50;
+                color: white;
+                font-size: 14px;
+                padding: 15px 20px;
+                margin: 8px 0;
+                border-radius: 4px;
+            }
+            </style>
+            <h1> Sample Escalation Tool </h1>
+            <p>
+            This page enables you to escalate the number of samples from each
+            county during an audit. That is, we assume that there is some set of
+            samples <i>s</i>. In the beginning of an audit, this set might be empty.
+            If the samples don't satisfy the relevant risk limit,
+            this tool provides a way to choose how many additional samples should be
+            chosen from each county.</p>
+
+            <p>In particular, we assume that we have the required number of additional
+            samples, <i>k</i> to get in the next round. We also know the number of total votes
+            in each county of our audit. In the single-county case, we would simply
+            poll <i>k</i> additional ballots from the county. This tool is for use
+            in the multiple county case.</p>
+
+            <p>For instance, let's assume we have 4 votes
+            left in county 1 and 2 votes left in county 2. We can represent this
+            as the list:
+            </p>
+
+            <p><tt>[1, 1, 1, 1, 2, 2]</tt></p>
+
+            <p>Then, we shuffle this list, randomly, using the audit seed set below.
+            For the default audit seed of 1, this gives us the shuffled list:
+            </p>
+            <p><tt>[1, 2, 2, 1, 1, 1]</tt></p>
+            <p>This implies that if we wanted to get one 
+            additional sample, we would get it from County 1. If we wanted two additional
+            samples, we would get 1 sample from County 1 and 1 sample from County 2,
+            and so on. </p>
+
+            <form action="GetSampleSize" method="GET">
+
+            <h2>Step 1: Enter County Names</h2>
+
+            <p>In the box below, enter the names of each county that is being audited
+            as a comma-separated list.
+            </p>
+            <p>
+            Example:
+            <tt> County A, County B </tt>
+            </p>
+
+            County Names: <input type="text" name="county_names" />
+
+            <h2>Step 2: Enter Total Votes Per County</h2>
+
+            <p>In the box below, enter the total number of ballots in each county,
+            including those already sampled, as a comma-separated list.</p>
+
+            <p>
+            Example:
+            <tt>1500, 2000</tt>
+            </p>
+
+            Number of Ballots per County: <input type="text" name="votes_per_county" />
+
+
+            <h2>Step 3: Enter Current Sample Sizes</h2>
+
+            <p> 
+            In the box below, 
+            enter the sample size in each county as a comma-separated list. If no
+            samples have been collected yet, this box can be left blank.
+            </p>
+
+            <p>
+            Example:
+            <tt>50, 75</tt>
+            </p>
+            
+            Current Sample Sizes: <input type="text" name="current_samples" />
+
+            <h2> Step 4: Enter Additional Required Sample Size</h2>
+
+            <p> In the box below, enter the total number of additional ballots to sample,
+            over all the counties, for the next stage in the audit. This should be
+            an integer value. </p>
+
+            <p>
+            Example:
+            <tt>50</tt>
+            </p>
+
+            Additional Sample Size: <input type="text" name="additional_sample_size" />
+
+            <h2>(Optional) Specify random number seed</h2>
+            <p>
+            The computation uses a random number seed, which defaults to 1.
+            You may if you wish enter a different seed here.
+            (Using the same seed with the same data always returns the same results.)
+            This is an optional parameter; there should be no reason to change it.
+            </p>
+
+            Seed: <input type="text" name="seed" />
+
+        
+            <h2>Compute results</h2>
+            Click on the "Submit" button below to compute the desired answers,
+            which will be shown on a separate page.
+            <input type="submit" />
+
+            </form>
+            '''
+
+    @cherrypy.expose
+    def GetSampleSize(
+        self, county_names,
+        current_samples, votes_per_county,
+        additional_sample_size, seed
+    ):
+        '''
+        Given the required additional sample size, the current samples in each county
+        and the total votes in each county, randomly allocate how many additional
+        samples to poll from each county.
+
+        In particular, if there are n_i ballots left over in county i, make a list containing
+        n_i instances of the number i. Then, shuffle the list, and 
+        given an additional sample size of k, choose the first k elements. In this prefix,
+        if there are s_i instances of the number i, then poll an additional s_i ballots
+        from county i.
+        '''
+        try:
+            if votes_per_county == "":
+                raise AssertionError("The total number of votes in each county must \
+                    be specified.")
+            
+            votes_per_county = [int(k.strip()) for k in votes_per_county.split(',')]
+
+            if current_samples == "":
+                current_samples = [0]*len(votes_per_county)
+            else:
+                current_samples = [int(k.strip()) for k in current_samples.split(',')]
+
+            if additional_sample_size == "":
+                raise AssertionError("The required additional sample size for the next \
+                    stage in the ballot must be specified as an integer value.")
+
+            additional_sample_size = int(additional_sample_size)
+
+            assert(len(current_samples) == len(votes_per_county)), "The length of the \
+                input list for current sample sizes should equal the length of the input list \
+                for the total votes per county. In particular, these values should both \
+                equal the number of counties in the audit."
+
+            county_names = [k.strip() for k in county_names.split(',')]
+
+            assert(len(county_names) == len(votes_per_county)), "The length of the input list \
+                for the total number of votes in each county should equal the number of counties \
+                specified in Step 1."
+
+            votes_tally = []
+            for county in range(len(current_samples)):
+                leftover_votes = votes_per_county[county] - current_samples[county]
+                assert(leftover_votes >= 0), "The number of total votes in a county cannot \
+                    exceed the number of votes in the sample. This is violated for county {}".format(
+                        county + 1)
+                votes_tally.extend([county+1]*leftover_votes)
+
+            if seed == "":
+                seed = 1
+            else:
+                seed = int(seed)
+
+            rs = bptool.create_rs(seed)
+            rs.shuffle(votes_tally)
+
+            additional_samples = votes_tally[:additional_sample_size]
+
+            return self.get_html_results(additional_samples, county_names)
+
+        except Exception as e:
+            return 'Please make sure all inputs are following the correct format and \
+            re-enter them <a href="./">here</a>.<br> \
+            The submitted form caused the following error message: <br> {}'.format(e)
+
+    def get_html_results(self, additional_samples, county_names):
+        '''
+        Given the county names and the appropriately sized prefix of how many
+        additional samples to poll from each county, print out the number of
+        ballots to poll from each county as an HTML table.
+
+        Input Parameters:
+
+        -additional_samples is a list of integers, where each integer
+        in the list ranges from 1 to the total number of counties.
+        -county_names is a list of length total number of counties, where
+        each element is a string representing the name of a county.
+
+        Returns:
+
+        -String rendering an HTML table representing how many additional
+        ballots need to be polled from each county.
+        '''
+        additional_per_county_tally = {}
+        for county in additional_samples:
+            additional_per_county_tally[county] = additional_per_county_tally.get(
+                county, 0) + 1
+
+        county_index_to_name_map = dict(enumerate(county_names))
+
+        results_str = (
+            '<style> \
+            table, th, td { \
+                     border: 1px solid black; \
+            }\
+            </style>\
+            <h1> Sample Escalation Tool </h1>')
+
+
+        results_str += '<table style="width:100%">'
+        results_str += '<tr>'
+        results_str += ("<th>{:<24s}</th> <th>{:<s}</th>"
+              .format("County Name",
+                      "Number of Additional Ballots to Sample"))
+        results_str += '</tr>'
+
+        for county in range(len(county_names)):
+            results_str += '<tr>'
+            results_str += ('<td style="text-align:center">{}</td>').format(
+                county_index_to_name_map[county])
+            results_str += ('<td style="text-align:center">{}</td>').format(
+                additional_per_county_tally[county+1])
+            results_str += '</tr>'
+        results_str += '</table>'
+        results_str += '<p> Click <a href="./">here</a> to go back to the main page.</p>'
+        return results_str
+
 
 server_conf = os.path.join(os.path.dirname(__file__), 'server_conf.conf')
 
